f6949ed327803786eb1fe687ce5d7131706db8e7
Si HaMaDa <nrookman_27@yahoo.com>
2017-03-01 12:48:37 +0200

    Admin panel starting...

---
+++ admin/index.php
@@ -0,0 +1,7 @@
+<?php
+include "header.php";
+
+include "side.php";
+
+include "footer.php";
+?>
---
+++ admin/side.php
@@ -0,0 +1,67 @@
+
+<!--sidebar start-->
+<aside>
+  <div id="sidebar"  class="nav-collapse ">
+      <!-- sidebar menu start-->
+      <ul class="sidebar-menu">                
+          <li class="active">
+              <a class="" href="index.php">
+                  <i class="icon_house_alt"></i>
+                  <span>Dashboard</span>
+              </a>
+          </li>
+          <li class="sub-menu">
+              <a href="javascript:;" class="">
+                  <i class="icon_document_alt"></i>
+                  <span>Products</span>
+                  <span class="menu-arrow arrow_carrot-right"></span>
+              </a>
+              <ul class="sub">
+                  <li><a class="" href="javascript:;">Add product</a></li>                          
+                  <li><a class="" href="javascript:;">Manage Products</a></li>
+              </ul>
+          </li>       
+          <li class="sub-menu">
+              <a href="javascript:;" class="">
+                  <i class="icon_desktop"></i>
+                  <span>Outer Products</span>
+                  <span class="menu-arrow arrow_carrot-right"></span>
+              </a>
+              <ul class="sub">
+                  <li><a class="" href="javascript:;">Add Product</a></li>
+                  <li><a class="" href="javascript:;">Manage Products</a></li>
+              </ul>
+          </li>
+          <li>
+              <a class="" href="javascript:;">
+                  <i class="icon_genius"></i>
+                  <span>Info</span>
+              </a>
+          </li>
+          <li>                     
+              <a class="" href="chart-chartjs.html">
+                  <i class="icon_piechart"></i>
+                  <span>Messages</span>
+                  
+              </a>
+                                 
+          </li>
+                     
+          <li class="sub-menu">
+              <a href="javascript:;" class="">
+                  <i class="icon_table"></i>
+                  <span>Users</span>
+                  <span class="menu-arrow arrow_carrot-right"></span>
+              </a>
+              <ul class="sub">
+                  <li><a class="" href="javascript:;">Add user</a></li>
+                  <li><a class="" href="javascript:;">Manage Users</a></li>
+              </ul>
+          </li>
+          
+          
+      </ul>
+      <!-- sidebar menu end-->
+  </div>
+</aside>
+<!--sidebar end-->
